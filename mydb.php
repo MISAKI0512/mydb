b7da6daa6d49932b31452c2b6e0961a17314292f
MISAKI0512 <jakkanne.0512@gmail.com>
2022-10-14 00:06:46 +0900

    first-commit

---
+++ mydb.php
@@ -0,0 +1,21 @@
+<?php
+ 
+$dsn = 'mysql:dbname=mydb;host=localhost';
+$user = 'root';
+$password = null;
+//※XAMPPを使用している場合は、passwordの中身は空にしてください。
+
+try {
+  $dbh = new PDO($dsn, $user, $password);
+  echo("接続に成功しました。<br>");
+} catch (PDOException $e) {
+  echo('Error:'.$e->getMessage());
+}
+
+$count = $dbh->exec('INSERT INTO items SET id=1, item_name="peach",price=210, keyword="缶詰,ピンク,甘い"');
+echo $count . '件のデータを挿入しました。';
+
+$dbh = null;
+
+
+?>
